81b4db4cd1da23dbf744bcd75983866a005743c2
Valo <vciuccio@frba.utn.edu.ar>
2026-04-13 20:27:17 -0300

    Add files via upload

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void){
+    printf("Hello, World!\n");
+
+    // Funcion para mostrar la version del compilador
+    printf("Compilado con la version de C: %ld\n", __STDC_VERSION__);
+}
